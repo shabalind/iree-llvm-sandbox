927e629c8baa56593be0f18160009b24bd03d7de
Denys Shabalin <shabalin@google.com>
2021-03-24 16:24:46 +0100

    More scaffolding

--- python/invoke.py
+++ python/invoke.py
@@ -1,3 +1,4 @@
+import ctypes
 import mlir.ir as ir
 import mlir.execution_engine as ee
 import mlir.dialects.linalg.opdsl.lang as tc
@@ -23,5 +24,39 @@ def matmul(A=tc.TensorDef(T1, S.M, S.K),
   C[D.m, D.n] += tc.cast(U, A[D.m, D.k]) * tc.cast(U, B[D.k, D.n])
 
 
+def to_mlir(op):
+  return r"""
+func @entrypoint() -> f64 attributes { llvm.emit_c_interface } {
+  %t_start = call @rtclock() : () -> f64
+  %t_end = call @rtclock() : () -> f64
+  %t_delta = subf %t_end, %t_start: f64
+  return %t_delta : f64
+}
+func @rtclock() -> f64 {
+  %zero = constant 0.0 : f64
+  return %zero : f64
+}
+  """
+
+
+def to_llvm(module):
+  import mlir.conversions
+  import mlir.passmanager as pm
+  pm = pm.PassManager.parse("convert-std-to-llvm")
+  pm.run(module)
+  print(module)
+  return module
+
+
 def invoke(op):
-  pass
+  with ir.Context():
+    module = ir.Module.parse(to_mlir(op))
+    engine = ee.ExecutionEngine(to_llvm(module))
+    c_double_p = ctypes.c_double * 1
+    res = c_double_p(-1.)
+    engine.invoke("entrypoint", res)
+    return res
+
+
+if __name__ == "__main__":
+  invoke(matmul)
